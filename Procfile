9a6c0fa1f20e316eb545f088a1d848b8a0a7c4e4
nnguyentrieuvy <nnguyentrieuvy1200@gmail.com>
2022-06-05 18:32:10 +0700

    update staticfiles

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn daugia.wsgi
+web: gunicorn daugia
--- daugia/settings.py
+++ daugia/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -160,7 +161,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
